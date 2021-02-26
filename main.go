04cc49f45eff5ebd7a3f25e6296af4799675773f
Loïc Sapone <loic@sapone.fr>
2021-02-26 16:32:42 +0100

    Remove date from build informations

--- main.go
+++ main.go
@@ -1,28 +1,13 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/iq2i/aergie/cmd"
 	"github.com/iq2i/aergie/internal/config"
 )
 
-var (
-	version = "dev"
-	date    = ""
-)
+var version = "DEV"
 
 func main() {
 	config.Init()
-	cmd.Execute(
-		buildVersion(version, date),
-	)
-}
-
-func buildVersion(version string, date string) string {
-	var result = version
-	if date != "" {
-		result = fmt.Sprintf("%s\nbuilt at: %s", result, date)
-	}
-	return result
+	cmd.Execute(version)
 }
